43cc28e29394418b894cc46b1deb067876e70906
taijusugahara <96ayutuki@gmail.com>
2021-05-24 10:55:11 +0900

    テストコード1回目

--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -3,7 +3,21 @@ require 'rails_helper'
 RSpec.describe "StaticPages", type: :system do
   
 
-  it '' do
+  it 'should get home' do
+    visit static_pages_home_path
+    expect(current_path).to eq (static_pages_home_path)
+
+  end
+
+  it 'should get help' do
+    visit static_pages_help_path
+    expect(current_path).to eq (static_pages_help_path)
+
+  end
+
+  it 'should get about' do
+    visit static_pages_about_path
+    expect(current_path).to eq (static_pages_about_path)
 
   end
 end
