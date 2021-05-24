e480da925066ae630ddc46f72e346d6a4f5b5560
taijusugahara <96ayutuki@gmail.com>
2021-05-24 11:18:26 +0900

    タイトル　テスト

--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -2,22 +2,28 @@ require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :system do
   
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
 
   it 'should get home' do
     visit static_pages_home_path
     expect(current_path).to eq (static_pages_home_path)
+    expect(page).to have_title "Home | #{@base_title}"
 
   end
 
   it 'should get help' do
     visit static_pages_help_path
     expect(current_path).to eq (static_pages_help_path)
+    expect(page).to have_title "Help | #{@base_title}"
 
   end
 
   it 'should get about' do
     visit static_pages_about_path
     expect(current_path).to eq (static_pages_about_path)
+    expect(page).to have_title "About | #{@base_title}"
 
   end
 end
