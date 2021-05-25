9a680a945ae206600d74ebc6f94db86fe35a9567
taijusugahara <96ayutuki@gmail.com>
2021-05-25 09:38:01 +0900

    length validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,19 @@ RSpec.describe User, type: :model do
     @user.name = "     "
     expect(@user).not_to be_valid
   end
+
+  it "email should be present" do
+    @user.email = "     "
+    expect(@user).not_to be_valid
+  end
+
+  it "name should not be too long" do
+    @user.name = "a" * 51
+    expect(@user).not_to be_valid
+  end
+
+  it "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    expect(@user).not_to be_valid
+  end
 end
