adc6f5ab86143aac42cd85678b0db0950f89b05f
taijusugahara <96ayutuki@gmail.com>
2021-05-25 13:50:46 +0900

    createアクション

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,12 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    @user = User.new(user_params)
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new'
+    end
   end
 
 
@@ -19,5 +24,12 @@ class UsersController < ApplicationController
     
   end
 
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 
 end
