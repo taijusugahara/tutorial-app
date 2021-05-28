fdefe0db896e5786b893a017a17ea0ed4760867c
taijusugahara <96ayutuki@gmail.com>
2021-05-28 16:20:17 +0900

    create

--- spec/system/microposts_spec.rb
+++ spec/system/microposts_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "Microposts", type: :system do
+RSpec.describe "Users", type: :system do
   before do
     @user = FactoryBot.create(:user)
-    # @microposts = FactoryBot.create(:micropost)
     # @microposts = FactoryBot.create_list(:micropost, 32)
     34.times do
     content = Faker::Lorem.sentence(word_count: 5)
@@ -20,5 +19,33 @@ RSpec.describe "Microposts", type: :system do
       expect(page).to have_content(micropost.content)
     end
  end
+
+end
+
+RSpec.describe "Microposts", type: :request do
+  before do
+    @micropost = FactoryBot.create(:micropost)
+    # @user = FactoryBot.create(:user)
+  end
+# ****今気づいたけどuserとmicropostはネストの関係じゃないんだ。だからuserのidとかいらない
+                      #  ここではそういう風にしてない
+  it 'should redirect create when not logged in' do
+    expect do
+      post microposts_path, params: { micropost: { content: "Lorem ipsum" } }
+              # こことかuserのidいらない
+    end.to change { Micropost.count }.by(0)
+     expect(response).to redirect_to login_url
+
+  end
+
+  it 'should redirect destroy when not logged in' do 
+    expect do
+      delete micropost_path(@micropost)
+                              # こことか
+    end.to change { Micropost.count }.by(0)
+     expect(response).to redirect_to login_url
+
+  end
+
 
 end
